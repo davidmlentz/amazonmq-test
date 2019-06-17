bd0a64913bf46da9ab054a1aa23fb458eeb3235d
David M. Lentz <davidmlentz@users.noreply.github.com>
2019-06-17 09:45:32 -0600

    Create send.js

---
+++ send.js
@@ -0,0 +1,40 @@
+var stompit = require('stompit');
+
+var connectParams = {
+    host: '<MY_MQ_HOSTNAME>',
+    port: 61614,
+    ssl: true,
+    connectHeaders:{
+        host: '<MY_MQ_HOSTNAME>',
+        login: '<MY_MQ_LOGIN>',
+        passcode: '<MY_MQ_PASSWORD>'
+    }
+};
+
+stompit.connect(connectParams, function(error, client){
+
+    if(error){
+        console.log('Unable to connect: ' + error.message);
+        return;
+    }
+
+    var sendParams = {
+        'destination': '/path/to/endpoint',
+        'content-type': 'application/json'
+    };
+
+    var frame = client.send(sendParams);
+
+    frame.end(JSON.stringify({
+        anything: 'anything',
+        example: true
+    }));
+
+    client.disconnect(function(error){
+        if(error){
+            console.log('Error while disconnecting: ' + error.message);
+            return;
+        }
+        console.log('Sent message');
+    });
+});
