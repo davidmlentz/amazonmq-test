5bb6482da725288cead8f244f3eed8fc34b99c6c
David M. Lentz <davidmlentz@users.noreply.github.com>
2019-08-19 16:34:58 -0600

    Update receive.js

--- receive.js
+++ receive.js
@@ -28,9 +28,9 @@ stompit.connect(connectParams, function(error, client){
     client.subscribe(subscribeParams, function(error, message){
         
         // Don't consume more than one message
-        if(consuming){
-            return;
-        }
+//        if(consuming){
+//            return;
+//        }
         
         consuming = true;
         
@@ -45,7 +45,7 @@ stompit.connect(connectParams, function(error, client){
         
         message.on('end', function(){
             client.ack(message);
-            client.disconnect();
+//            client.disconnect();
         });
     });
 });
